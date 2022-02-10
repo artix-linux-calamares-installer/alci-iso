bd4bea9d706890fd7e5e0ce3dcb860bb165399c7
Shinyzenith <aakashsensharma@gmail.com>
2022-02-11 01:20:38 +0530

    fix script

--- artixiso/artools-workspace/iso-profiles/build.sh
+++ artixiso/artools-workspace/iso-profiles/build.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-non_root_user=liveuser # since makepkg doesn't run as root, we need a non root user for aur package building ( make sure the user has no password )
+non_root_user=artix # since makepkg doesn't run as root, we need a non root user for aur package building ( make sure the user has no password )
 init=runit
 #init=dinit
 #init=s6
 #init=suite66
 #init=openrc
 
+trap "" EXIT
 buildiso -i $init -p base -x
 
-artix-chroot /var/lib/artools/buildiso/base/artix/rootfs bash -c "pacman-key --init; pacman-key --populate artix;pacman-key --populate archlinux"
+artix-chroot /var/lib/artools/buildiso/base/artix/rootfs bash -c "pacman-key --init; pacman-key --populate artix;pacman-key --populate archlinux; pacman -Syy"
 
 printf "\033[1;33mAUR_BUILD:\033[0m Reading package list.\n"
 for package in $(cat ./AUR_PACKAGES | grep -vE "^#.*$")
