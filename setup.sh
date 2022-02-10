b8d09cefe9d08cf3c35cf25410ff752b2346a7bf
Shinyzenith <aakashsensharma@gmail.com>
2022-02-10 07:49:27 +0530

    [update] Change directory structure.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 sudo modprobe loop
 sudo pacman -S --noconfirm --needed artools iso-profiles
-ln -sf "$(pwd)/artools-workspace" ~/
-ln -sf "$(pwd)/artools" ~/.config/
+ln -sf "$(pwd)/artixiso/artools-workspace" ~/
+ln -sf "$(pwd)/config/artools" ~/.config/
