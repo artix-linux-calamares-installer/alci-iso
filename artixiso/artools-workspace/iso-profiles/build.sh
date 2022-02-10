09d20cfff875fbded6e9acae38eceb9e4ed2b20b
Shinyzenith <aakashsensharma@gmail.com>
2022-02-11 00:25:02 +0530

    incomplete build script

--- artixiso/artools-workspace/iso-profiles/build.sh
+++ artixiso/artools-workspace/iso-profiles/build.sh
@@ -1,2 +1,41 @@
 #1/usr/bin/env bash
-buildiso -i runit -p base
+
+non_root_user=liveuser # since makepkg doesn't run as root, we need a non root user for aur package building ( make sure the user has no password )
+init=runit
+#init=dinit
+#init=s6
+#init=suite66
+#init=openrc
+
+buildiso -i $init -p base -x
+artix-chroot /var/lib/artools/buildiso/base/artix/rootfs pacman-key --init
+artix-chroot /var/lib/artools/buildiso/base/artix/rootfs pacman-key --populate artix
+artix-chroot /var/lib/artools/buildiso/base/artix/rootfs pacman-key --populate archlinux
+
+printf "\033[1;33mAUR_BUILD:\033[0m Reading package list.\n"
+for package in $(cat ./AUR_PACKAGES | grep -vE "^#.*$")
+do
+  printf "\033[1;33mAUR_BUILD:\033[0m Cloning ${package}.\n"
+  artix-chroot /var/lib/artools/buildiso/base/artix/rootfs \
+	  git clone https://aur.archlinux.org/${package}.git /etc/aur_building/$package
+done
+
+printf "\033[1;33mAUR_BUILD:\033[0m Building packages.\n"
+
+artix-chroot /var/lib/artools/buildiso/base/artix/rootfs chmod 777 -R /etc/aur_building/paru-bin
+dirs=$(artix-chroot /var/lib/artools/buildiso/base/artix/rootfs find /etc/aur_building -mindepth 1 -maxdepth 1 -type d)
+for dir in $dirs
+do
+	artix-chroot /var/lib/artools/buildiso/base/artix/rootfs bash -c "cd $dir; sudo -u $non_root_user makepkg -sfci --noconfirm --needed"
+done
+
+if [[ $(artix-chroot /var/lib/artools/buildiso/base/artix/rootfs pacman -Qtdq) != "" ]];then
+    artix-chroot /var/lib/artools/buildiso/base/artix/rootfs \ 
+		yes | sudo pacman -Rns $(pacman -Qtdq)
+    printf "\u001b[31mPACMAN:\033[0m Orphans found and removed, exiting script.\n"
+else 
+    printf "\u001b[31mPACMAN:\033[0m No orphans found, exiting script.\n"
+fi
+rm -rf /etc/aur_building
+
+buildiso -i $init -p base -sc
