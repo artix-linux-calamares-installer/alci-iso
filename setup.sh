bded2f3dc4c3cc2f0da55ad7ad76a12fb59bd451
Shinyzenith <aakashsensharma@gmail.com>
2022-02-11 11:43:11 +0530

    Delete liveuser on completion

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 sudo modprobe loop
 sudo pacman -S --noconfirm --needed artools iso-profiles
+sudo pacman -S --noconfirm --needed virtualbox virtualbox-host-dkms
+sudo modprobe vboxdrv
 ln -sf "$(pwd)/artixiso/artools-workspace" ~/
 ln -sf "$(pwd)/config/artools" ~/.config/
