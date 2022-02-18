e7bf679e60d8fa069b6213118714dee7f9891087
Shinyzenith <aakashsensharma@gmail.com>
2022-02-18 21:52:18 +0530

    change order

--- artixiso/artools-workspace/iso-profiles/build.sh
+++ artixiso/artools-workspace/iso-profiles/build.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 non_root_user=artix # since makepkg doesn't run as root, we need a non root user for aur package building ( make sure the user has no password )
-init=runit
 #init=dinit
+#init=openrc
+init=runit
 #init=s6
 #init=suite66
-#init=openrc
 
 trap "" EXIT
 buildiso -i $init -p base -x
