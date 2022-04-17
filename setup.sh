c5f54fbf53d79fe6d67070acb2f677238710cfb7
Kungger-git <matthewriel63@gmail.com>
2022-04-17 20:39:02 +0800

    feat: add packages to install on setup: `archlinux-keyring, archlinux-mirrorlist` to avoid missing mirrorlist errors during build.

--- setup.sh
+++ setup.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 sudo modprobe loop
-sudo pacman -S --noconfirm --needed artools iso-profiles
-sudo pacman -S --noconfirm --needed virtualbox virtualbox-host-dkms
-sudo modprobe vboxdrv
+
+sudo pacman -Syyu --noconfirm
+sudo pacman -S --noconfirm --needed artools iso-profiles archlinux-keyring archlinux-mirrorlist
+
+sudo pacman-key --init
+sudo pacman-key --populate
+
 ln -sf "$(pwd)/artixiso/artools-workspace" ~/
 ln -sf "$(pwd)/config/artools" ~/.config/
